9fb237acd5056270ad745c80374a2f77ba028eff
vasia <vasilikikalavri@gmail.com>
2015-01-15 11:48:29 +0100

    unweighted; single-source; filters out referers

--- src/main/java/flink/graphs/example/RelevanceSearch.java
+++ src/main/java/flink/graphs/example/RelevanceSearch.java
@@ -1,15 +1,15 @@
 package flink.graphs.example;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.flink.api.common.ProgramDescription;
 import org.apache.flink.api.common.functions.FlatJoinFunction;
+import org.apache.flink.api.common.functions.GroupReduceFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple1;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.spargel.java.MessageIterator;
 import org.apache.flink.spargel.java.MessagingFunction;
 import org.apache.flink.spargel.java.OutgoingEdge;
@@ -26,12 +26,14 @@ import flink.graphs.Vertex;
  * Given a bipartite graph with vertex groups V1, V2 and a set of k source nodes in group V1, 
  * the algorithm computes relevance scores to the k source nodes for all other nodes in V1. 
  *
+ * The implementation is based on the paper "Relevance search and anomaly detection in bipartite graphs"
+ * SIGKDD, December 2005.
  */
 public class RelevanceSearch implements ProgramDescription {
 
-	private static int k; // the number of given sources
-	private static Long[] sources; // the source ids
-	private final static float probC = 0.15f; // the restarting probability 
+	private static long source; // the source id
+	private final static float probC = 0.15f; // the restarting probability
+	private static int maxIterations;
 
 	@Override
 	public String getDescription() {
@@ -41,28 +43,26 @@ public class RelevanceSearch implements ProgramDescription {
 	@SuppressWarnings("serial")
 	public static void main(String[] args) throws Exception {
 		if (args.length < 4) {
-			System.err.println("Usage: Relevance Search <input-edges> <number-of-sources> <source-ids> <output-path>");
+			System.err.println("Usage: Relevance Search <input-edges> <number-of-iterations> <source-ids> <output-path>");
 		}
 
 		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-		k = Integer.parseInt(args[1]);
-		sources = new Long[k];
-		sources[0] = Long.parseLong(args[2]);
+		maxIterations = Integer.parseInt(args[1]);
+		source = Long.parseLong(args[2]);
 
 		/** read the edges input **/
 		DataSet<Edge<Long, Double>> edges = env.readCsvFile(args[0]).fieldDelimiter('\t').lineDelimiter("\n")
 				.types(Long.class, Long.class).map(new InitEdgesMapper()); 
-//				getDirectedEdgesDataSet(env);
 		
 		/** create the referers vertex group **/
-		DataSet<Vertex<Long, Double[]>> referers = getReferersDataSet(edges);
+		DataSet<Vertex<Long, Double>> referers = getReferersDataSet(edges);
 
 		/** create the hosts vertex group **/
-		DataSet<Vertex<Long, Double[]>> hosts = getHostsDataSet(edges);
+		DataSet<Vertex<Long, Double>> hosts = getHostsDataSet(edges);
 
 		/** create the graph **/
-		Graph<Long, Double[], Double> graph = Graph.create(referers.union(hosts), edges, env).getUndirected();
+		Graph<Long, Double, Double> graph = Graph.create(referers.union(hosts), edges, env).getUndirected();
 		
 		/** scale the edge weights by dividing each edge weight with the sum of weights of all out-edges */
 		DataSet<Tuple2<Long, Long>> outDegrees = graph.outDegrees();
@@ -76,13 +76,41 @@ public class RelevanceSearch implements ProgramDescription {
 		});
 
 		/** run the iterative update of relevance scores */
-		Graph<Long, Double[], Double> scaledGraph = Graph.create(graph.getVertices(), scaledEdges, env);
+		Graph<Long, Double, Double> scaledGraph = Graph.create(graph.getVertices(), scaledEdges, env);
 		
-		/** store the output **/
-		scaledGraph.runVertexCentricIteration(new ComputeRelevanceScores(), new SendNewScores(), 1)
-		.getVertices().writeAsCsv(args[3], "\n", "\t");
-		//.print();
+		/** compute the relevance scores **/
+		DataSet<Vertex<Long, Double>> scaledScoredVertices = scaledGraph.runVertexCentricIteration(
+				new ComputeRelevanceScores(), 
+				new SendNewScores(), maxIterations)
+				.getVertices();
 
+		/** filter out the referers */
+		scaledScoredVertices
+		.join(hosts).where(0).equalTo(0).with(
+				new FlatJoinFunction<Vertex<Long,Double>, Vertex<Long,Double>, Vertex<Long,Double>>() {
+					public void join(Vertex<Long, Double> first,
+							Vertex<Long, Double> second,
+							Collector<Vertex<Long, Double>> out) {
+						out.collect(first);
+					}
+		})
+		/** order by relevance score */
+		.map(new MapFunction<Vertex<Long, Double>, Tuple3<Integer, Long, Double>>() {
+			public Tuple3<Integer, Long, Double> map(Vertex<Long, Double> vertex) {
+				return new Tuple3<Integer, Long, Double>(42, vertex.getId(), vertex.getValue());
+			}
+		}).groupBy(0).sortGroup(2, Order.DESCENDING).reduceGroup(
+				new GroupReduceFunction<Tuple3<Integer,Long,Double>, Vertex<Long, Double>>() {
+					public void reduce(
+							Iterable<Tuple3<Integer, Long, Double>> values,
+							Collector<Vertex<Long, Double>> out) {
+						for (Tuple3<Integer, Long, Double> value : values) {
+							out.collect(new Vertex<Long, Double>(value.f1, value.f2));
+						}
+					}
+		})
+		/** store the output **/
+		.writeAsCsv(args[3], "\n", "\t");
 		env.execute();
 	}
 
@@ -94,57 +122,38 @@ public class RelevanceSearch implements ProgramDescription {
 	}
 	
 	@SuppressWarnings("serial")
-	public static final class ComputeRelevanceScores extends VertexUpdateFunction<Long, Double[], Double[]> {
-		public void updateVertex(Long vertexKey, Double[] vertexValue, MessageIterator<Double[]> inMessages) {
-			Double[] newScores = new Double[k];
-			for (int i=0; i<k; i++) {
-				newScores[i] = 0.0;
-			}
-			for (Double[] message : inMessages) {
-				for (int i=0; i<k; i++) {
-					newScores[i] += message[i];
-				}
+	public static final class ComputeRelevanceScores extends VertexUpdateFunction<Long, Double, Double> {
+		public void updateVertex(Long vertexKey, Double vertexValue, MessageIterator<Double> inMessages) {
+			double newScore = 0.0;
+
+			for (Double message : inMessages) {
+					newScore += message;
 			}
 			// add the q vector value if needed
-			for (int i=0; i<k; i++) {
-				if (sources[i] == vertexKey) {
-					newScores[i] += probC;
-					break;
-				}
+			if (vertexKey.equals(source)) {
+					newScore += probC;
 			}
-			setNewVertexValue(newScores);
+			setNewVertexValue(newScore);
 		}
 	}
 	
 	@SuppressWarnings("serial")
-	public static final class SendNewScores extends MessagingFunction<Long, Double[], Double[], Double> {
-		public void sendMessages(Long vertexKey, Double[] vertexValue) {
+	public static final class SendNewScores extends MessagingFunction<Long, Double, Double, Double> {
+		public void sendMessages(Long vertexKey, Double vertexValue) {
 			// (1-c)*edgeValue*score
-			Double[] scaledScores = new Double[k];
 			 for (OutgoingEdge<Long, Double> edge : getOutgoingEdges()) {
-				 for(int i=0; i<k; i++) {
-					 scaledScores[i] = vertexValue[i]*edge.edgeValue()*(1-probC);
-				 }
-	                sendMessageTo(edge.target(), scaledScores);
+				 double scaledScore = vertexValue*edge.edgeValue()*(1-probC);
+	                sendMessageTo(edge.target(), scaledScore);
 	            }
 		}
-	}
-	
-	private static DataSet<Edge<Long, Double>> getDirectedEdgesDataSet(ExecutionEnvironment env) {
-		List<Edge<Long, Double>> edges = new ArrayList<Edge<Long, Double>>();
-		edges.add(new Edge<Long, Double>(1L, 5L, 1.0));
-		edges.add(new Edge<Long, Double>(1L, 6L, 1.0));
-		edges.add(new Edge<Long, Double>(2L, 5L, 1.0));
-		edges.add(new Edge<Long, Double>(3L, 6L, 1.0));
-		return env.fromCollection(edges);
 	}
 
 	/**
 	 * Pages having no incoming edges
 	 */
 	@SuppressWarnings("serial")
-	private static DataSet<Vertex<Long, Double[]>> getReferersDataSet(DataSet<Edge<Long, Double>> edges) {
-		DataSet<Vertex<Long, Double[]>> referers = edges.map(
+	private static DataSet<Vertex<Long, Double>> getReferersDataSet(DataSet<Edge<Long, Double>> edges) {
+		DataSet<Vertex<Long, Double>> referers = edges.map(
 				new MapFunction<Edge<Long, Double>, Tuple1<Long>>() {
 					public Tuple1<Long> map(Edge<Long, Double> edge) { return new Tuple1<Long>(edge.getSource()); }
 		}).distinct().map(new InitializeVertex());
@@ -155,8 +164,8 @@ public class RelevanceSearch implements ProgramDescription {
 	 * Pages having no outgoing edges
 	 */
 	@SuppressWarnings("serial")
-	private static DataSet<Vertex<Long, Double[]>> getHostsDataSet(DataSet<Edge<Long, Double>> edges) {
-		DataSet<Vertex<Long, Double[]>> hosts = edges.map(
+	private static DataSet<Vertex<Long, Double>> getHostsDataSet(DataSet<Edge<Long, Double>> edges) {
+		DataSet<Vertex<Long, Double>> hosts = edges.map(
 				new MapFunction<Edge<Long, Double>, Tuple1<Long>>() {
 					public Tuple1<Long> map(Edge<Long, Double> edge) { return new Tuple1<Long>(edge.getTarget()); }
 		}).distinct().map(new InitializeVertex());
@@ -164,13 +173,9 @@ public class RelevanceSearch implements ProgramDescription {
 	}
 	
 	@SuppressWarnings("serial")
-	public static final class InitializeVertex implements MapFunction<Tuple1<Long>, Vertex<Long, Double[]>> {
-		public Vertex<Long, Double[]> map(Tuple1<Long> vertexId) {
-			Double[] vertexValue = new Double[k];
-			for (int i=0; i<k; i++) {
-				vertexValue[i] = 0.0;
-			}
-			return new Vertex<Long, Double[]>(vertexId.f0, vertexValue);
+	public static final class InitializeVertex implements MapFunction<Tuple1<Long>, Vertex<Long, Double>> {
+		public Vertex<Long, Double> map(Tuple1<Long> vertexId) {
+			return new Vertex<Long, Double>(vertexId.f0, 0.0);
 		}
 	}
 }
